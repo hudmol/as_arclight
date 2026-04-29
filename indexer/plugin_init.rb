619b115857b94dce8da758948d3239d6d23e5d50
James Bullen <jambun@gmail.com>
2026-04-29 14:35:50 +1000

    start up checks for prefix configs

--- indexer/plugin_init.rb
+++ indexer/plugin_init.rb
@@ -8,6 +8,26 @@ end
 unless AppConfig.has_key?(:as_arclight_indexing_frequency_seconds)
   bad.push("as_arclight plugin requires AppConfig[:as_arclight_indexing_frequency_seconds] to be set. Example: 60")
 end
+if AppConfig.has_key?(:as_arclight_ead_id_prefix)
+  if AppConfig[:as_arclight_ead_id_prefix].is_a?(String)
+    if AppConfig[:as_arclight_ead_id_prefix].include?(' ')
+      # FIXME: other rules?
+      bad.push("as_arclight plugin AppConfig[:as_arclight_ead_id_prefix] cannot contain spaces")
+    end
+  else
+    bad.push("as_arclight plugin AppConfig[:as_arclight_ead_id_prefix] must be a String")
+  end
+end
+if AppConfig.has_key?(:as_arclight_ref_id_prefix)
+  if AppConfig[:as_arclight_ref_id_prefix].is_a?(String)
+    if AppConfig[:as_arclight_ref_id_prefix].include?(' ')
+      # FIXME: other rules?
+      bad.push("as_arclight plugin AppConfig[:as_arclight_ref_id_prefix] cannot contain spaces")
+    end
+  else
+    bad.push("as_arclight plugin AppConfig[:as_arclight_ref_id_prefix] must be a String")
+  end
+end
 unless bad.empty?
   raise bad.join("\n")
 end
