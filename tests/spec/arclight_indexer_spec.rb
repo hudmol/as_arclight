a50e8ba8725952244cbe862b2dc249643e9c4c27
James Bullen <jambun@gmail.com>
2026-06-22 14:48:47 +1000

    ArclightIndexer now sets data_dir on initialization

--- tests/spec/arclight_indexer_spec.rb
+++ tests/spec/arclight_indexer_spec.rb
@@ -10,8 +10,6 @@ describe 'ArclightIndexer' do
     end
 
     JSONModel::init(enum_source: mock_enum_source)
-
-    ArclightIndexer.data_dir = File.join(AppConfig[:data_directory], 'as_arclight')
   end
 
   let!(:indexer) do
