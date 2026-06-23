4e975da0d6c3b9e81ec492564daa5450b6ba1868
James Bullen <jambun@gmail.com>
2026-06-23 10:17:09 +1000

    one more test for index version

--- tests/spec/index_version_spec.rb
+++ tests/spec/index_version_spec.rb
@@ -1,8 +1,6 @@
 describe 'IndexVersion' do
 
-  def indexer_db
-    ArclightIndexer.prepare_db
-  end
+  let(:db) { ArclightIndexer.prepare_db }
 
   before(:each) do
     indexer_db[:index_version].delete
@@ -10,16 +8,12 @@ describe 'IndexVersion' do
 
   describe '#validate_config_or_die!' do
     it 'creates an initial index version on a first run' do
-      db = indexer_db
-
       allow(AppConfig).to receive(:[]).with(:as_arclight_index_version).and_return(1)
       IndexVersion.validate_config_or_die!(db)
       expect(db[:index_version].count).to eq(1)
     end
 
     it 'recommends a reindex if the index version has increased' do
-      db = indexer_db
-
       allow(AppConfig).to receive(:[]).with(:as_arclight_index_version).and_return(1)
       IndexVersion.validate_config_or_die!(db)
       allow(AppConfig).to receive(:[]).with(:as_arclight_index_version).and_return(2)
@@ -28,13 +22,18 @@ describe 'IndexVersion' do
     end
 
     it 'dies if the index version has decreased' do
-      db = indexer_db
-
       allow(AppConfig).to receive(:[]).with(:as_arclight_index_version).and_return(1)
       IndexVersion.validate_config_or_die!(db)
       allow(AppConfig).to receive(:[]).with(:as_arclight_index_version).and_return(0)
       expect{ IndexVersion.validate_config_or_die!(db) }.to raise_error(IndexVersion::ConfigurationError)
     end
-  end
+
+    it 'dies if the config has changed but the version has not' do
+      allow(AppConfig).to receive(:[]).with(:as_arclight_index_version).and_return(1)
+      IndexVersion.validate_config_or_die!(db)
+      allow(AppConfig).to receive(:[]).with(:as_arclight_resource_id_prefix).and_return('new prefix')
+      expect{ IndexVersion.validate_config_or_die!(db) }.to raise_error(IndexVersion::ConfigurationError)
+    end
+ end
 
 end
